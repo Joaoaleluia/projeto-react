12b10813e12bbb58ace90507d9b9a36260ab5822
Joaoaleluia <joaoaleluia7246ce@gmail.com>
2025-10-20 11:35:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import { DatabasePostgres } from '.databasePostgres';
+import './createTable.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dontenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const database = new DatabasePostgres();
+
+//cadastro
+app.post('/auth/register', async(req, res) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({ msg: 'Preeche todos os campos!'});
+    }
+    const existingUser = await database.findByEmail(email);
+    if(existingUser){
+        return res.status(400).json({ msg: 'Email já está cadastrado'});
+    }
+
+    await database.create({ name, email, password });
+    res.status(201).json({ msg: 'Usuário criado!'});
+})
+
+//Login
+app.post(' /auth/login', async(req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.status(400).json({ msg: 'Prencha email e senha!'});
+    }
+
+})
