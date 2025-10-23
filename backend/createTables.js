7e06c897be1980c0cce856d5ea6459286b99dd58
Joaoaleluia <joaoaleluia7246ce@gmail.com>
2025-10-23 07:59:51 -0300

    Add files via upload

--- backend/createTables.js
+++ backend/createTables.js
@@ -1,7 +1,7 @@
 import { sql } from './sql.js'
 
 await sql`
-    CREATE TABLE IF NOT EXISTS users(
+    CREATE TABLE IF NOT EXISTS users (
         id UUID PRIMARY KEY,
         name TEXT NOT NULL,
         email TEXT NOT NULL UNIQUE,
